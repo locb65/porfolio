f7358c95e7bf9aa0e1ccecc06a9e43b781ce1284
locb65 <locb65@gmail.com>
2023-06-10 12:12:51 -0400

    react app made and tailwindcss setup

---
+++ portfolio-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App mx-auto bg-blue-50 rounded-xl">
+      <p className="text-8xl text-gray-700 fond-bolder mb-5">Welcome</p>
+
+    </div>
+  );
+}
+
+export default App;
