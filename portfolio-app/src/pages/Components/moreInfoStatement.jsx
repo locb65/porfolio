d214e75d009f95dd321afac0c3bcba8177f47660
locb65 <locb65@gmail.com>
2023-10-18 21:18:20 -0400

    changed state text shadow

--- portfolio-app/src/pages/Components/moreInfoStatement.jsx
+++ portfolio-app/src/pages/Components/moreInfoStatement.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import statementBackground from '../../utils/imgs/statement-background.jpg'
 
 export const MoreInfoStatement = () => {
-  const style = {textShadow: ' 2px 2px 4px rgba(0, 0, 0, 0.8'}
+  const style = {textShadow: ' 3px 3px 6px rgba(255,255,255, 0.8'}
   return (
     <div className='opacity-90 w-full h-[250px] md:h-[350px] lg:h-[450px] xl:h-[550px] bg-cover bg-center' 
     style={{backgroundImage: `url(${statementBackground})`}}>
     
-    <div className='z-1 flex flex-col justify-center items-center content-center h-full text-5xl font-bold shadow-lg text-white' style = {style}>Revolutionizing software solutions for a brighter future!</div>
+    <div className='z-1 flex flex-col justify-center items-center content-center h-full text-5xl font-bold shadow-lg text-black' style = {style}>Revolutionizing software solutions for a brighter future!</div>
         
     </div>
   )
